e731b0683e6a3dcdfdda0a55274a25e101f76d3d
MD Monir <MD.Monir@thomsonreuters.com>
2021-09-02 18:47:39 +0800

    Commit Service and zrepo Inerface and Course View Model

---
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModels.cs
@@ -0,0 +1,11 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CleanArch.Application.ViewModels
+{
+    public class CourseViewModels
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
